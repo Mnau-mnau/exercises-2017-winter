b38ff1f965dd76f67e5e7fa2ae27ca5f4f93ac16
Michal Špaček <mail@michalspacek.cz>
2017-02-16 10:05:06 +0100

    Adding database export

---
+++ day24-mysql/export.sql
@@ -0,0 +1,65 @@
+-- Adminer 4.2.5 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `orders` (`id`, `user_id`, `created`) VALUES
+(1,	1,	'2017-02-09 16:46:06'),
+(2,	1,	'2017-02-09 16:57:08'),
+(3,	1,	'2017-02-09 16:57:38');
+
+DROP TABLE IF EXISTS `orders_have_products`;
+CREATE TABLE `orders_have_products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `order_id` (`order_id`),
+  KEY `product_id` (`product_id`),
+  CONSTRAINT `orders_have_products_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
+  CONSTRAINT `orders_have_products_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `orders_have_products` (`id`, `order_id`, `product_id`) VALUES
+(1,	3,	3);
+
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `price` decimal(12,2) unsigned NOT NULL,
+  `size` varchar(127) NOT NULL,
+  `color` varchar(127) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `products` (`id`, `name`, `price`, `size`, `color`) VALUES
+(1,	'name',	123.00,	'size',	'color'),
+(2,	'ewrwer',	123.00,	'3214324',	'324324'),
+(3,	'e',	433.00,	'12',	'234');
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `users` (`id`, `email`, `password`, `name`) VALUES
+(1,	'foo@bar.org',	'',	'Foo Bar');
+
+-- 2017-02-16 09:03:51
